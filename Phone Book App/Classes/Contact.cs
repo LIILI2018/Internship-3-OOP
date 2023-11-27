1c1718a89dabd935c1263c115f418de136a7c684
Lukaaaa <luka218393@gmail.com>
2023-11-28 00:05:38 +0100

    Added Classes directory, Enum subdirectory, created call and contact class, created calStatus and Preferences enum classes

---
+++ Phone Book App/Classes/Contact.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Phone_Book_App.Classes
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Surename { get; set; }
+        public int PhineNumber { get; set; }
+        public string Preferences { get; set; }
+
+    }
+}
---
+++ Phone Book App/Classes/Enums/Preferences.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phone_Book_App.Classes
+{
+	public enum Preferences
+	{
+		Favourite,
+		Normal,
+		Blocked
+	}
+}
